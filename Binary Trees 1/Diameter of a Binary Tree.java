0b747a4554c9f87700f96d9ae047f3e431196cfd
shaikjainulabdin <54411936+shaikjainulabdin@users.noreply.github.com>
2023-02-14 22:23:22 +0530

    Create Diameter of a Binary Tree.java

---
+++ Binary Trees 1/Diameter of a Binary Tree.java
@@ -0,0 +1,99 @@
+//     https://course.acciojob.com/idle?question=87b0d2e6-d58a-4149-a832-70355e59043b
+
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+
+
+class Main {
+    static Node buildTree(String str) {
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if (!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if (i >= ip.length) break;
+            currVal = ip[i];
+            if (!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        Node root1 = buildTree(s1);
+        Solution g = new Solution();
+        System.out.println(g.diameter(root1));
+    }
+}
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+       static  class Pair
+	   {
+	
+		int height;
+		int diameter;
+		Pair()
+		{
+	       height = 0;
+			diameter = 0;
+		}
+		Pair(int height, int diameter)
+	   {
+			    this.height = height;
+			   this.diameter = diameter;
+	}
+	public static Pair diameter_(Node root)
+	{
+		if(root == null)
+		{
+			return new Pair();
+		}
+		Pair leftSubTree = diameter_(root.left);
+		Pair rightSubTree = diameter_(root.right);
+		int diaThroughRoot = leftSubTree.height + rightSubTree.height + 1;
+		int myHeight = Math.max(leftSubTree.height , rightSubTree.height) + 1;
+		int myDiameter = Math.max(diaThroughRoot, Math.max(leftSubTree.diameter, rightSubTree.diameter));
+		return new Pair(myHeight, myDiameter);
+	}
+    
+    public static int diameter(Node root) {
+        // Your code here
+		Pair ans = diameter_(root);
+		return ans.diameter;
+    }
+}
